08a0892f65aa0352a0de9564de3b00d1fb137d80
santosh-sy <santoshoh9@gmail.com>
2025-11-27 12:41:32 -0500

    feat: add solution for Valid Parentheses

---
+++ 20_ValidParentheses/ValidParentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            elif len(stack) > 0 and char == ')' and stack[-1] == '(':
+                stack.pop()
+            elif len(stack) > 0 and char == '}' and stack[-1] == '{':
+                stack.pop()
+            elif len(stack) > 0 and char == ']' and stack[-1] == '[':
+                stack.pop()
+            else:
+                return False
+        return len(stack) == 0
